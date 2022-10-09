1bc8394797c257a13a0b487cef55558337d83adb
sefrolov <stanislav.evg.frolov@gmail.com>
2022-10-09 17:03:01 +0300

    Update TeleMecanum.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleMecanum.java
@@ -11,44 +11,42 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name = "main_tele_rev")
-public class sample extends LinearOpMode {
-    DcMotor MotorRF, MotorLF, MotorRB, MotorLB, liftMotor;
-    Servo servo;
-    boolean open = false;
-    double rotation_speed = 0, mod = 0;
-    double speedLF = 0, speedRF = 0, speedLB = 0, speedRB = 0;
-    ElapsedTime button_timer = new ElapsedTime();
-    //vec2 JoysticDir;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@TeleOp
+public class TeleMecanum extends LinearOpMode {
+    DcMotor MotorRF, MotorLF, MotorRB, MotorLB;
 
     public void runOpMode(){
         MotorLB = hardwareMap.get(DcMotor.class, "motor_lb");
         MotorRB = hardwareMap.get(DcMotor.class, "motor_rb");
         MotorLF = hardwareMap.get(DcMotor.class, "motor_lf");
         MotorRF = hardwareMap.get(DcMotor.class, "motor_rf");
-        liftMotor = hardwareMap.get(DcMotor.class, "lift_motor");
-        servo = hardwareMap.get(Servo.class, "cap");
 
         MotorRB.setDirection(DcMotorSimple.Direction.REVERSE);
         MotorRF.setDirection(DcMotorSimple.Direction.REVERSE);
-        liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        MotorLB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        MotorRB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        MotorLF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        MotorRF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        telemetry.addData("","Init complited");
+        telemetry.update();
 
         waitForStart();
-
-        button_timer.reset();
 
         while(opModeIsActive()){
             double y = -gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x;
             double rotate = gamepad1.right_trigger - gamepad1.left_trigger;
-            double lift = gamepad2.left_stick_y;
             double lf = y + x + rotate;
             double lb = y - x + rotate;
             double rf = y - x - rotate;
             double rb = y + x - rotate;
             double[] powers = {lf, lb, rf, rb};
             double max = 0;
-            for (int i = 0; i < 3; i++) {
+            for (int i = 0; i < 4; i++) {
                 if (Math.abs(powers[i]) > max)
                     max = Math.abs(powers[i]);
             }
@@ -62,7 +60,6 @@ public class sample extends LinearOpMode {
             MotorRB.setPower(rb);
             MotorLF.setPower(lf);
             MotorRF.setPower(rf);
-            liftMotor.setPower(lift);
             telemetry.addData("lb", lb);
             telemetry.addData("lf", lf);
             telemetry.addData("rf", rf);
