c541e15b22fa84166d5830076adfae9a9cb421d2
anne12332 <111519292+anne12332@users.noreply.github.com>
2023-02-15 19:37:38 +0300

    Add files via upload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewTele.java
@@ -3,8 +3,13 @@ package org.firstinspires.ftc.teamcode;
 import static java.lang.Math.PI;
 import static java.lang.Math.abs;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -13,6 +18,7 @@ public class NewTele extends LinearOpMode {
     RobotNW Robot = new RobotNW();
     ElapsedTime sensor_sleep = new ElapsedTime();
     Servo servo;
+    ColorSensor colorSensor;    // Hardware Device Object
     elevatorNewThreadMiddle new_thread = new elevatorNewThreadMiddle();
     double[] basePos = {0, 0, 0, 0};
     //double elevatorEncoder;
@@ -29,11 +35,34 @@ public class NewTele extends LinearOpMode {
         new_thread.init();
         new_thread.start();
 
+        float hsvValues[] = {0F,0F,0F};
+
+        // values is a reference to the hsvValues array.
+        final float values[] = hsvValues;
+
+        // get a reference to the RelativeLayout so we can change the background
+        // color of the Robot Controller app to match the hue detected by the RGB sensor.
+        int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
+        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+
+        // bPrevState and bCurrState represent the previous and current state of the button.
+        boolean bPrevState = false;
+        boolean bCurrState = false;
+
+        // bLedOn represents the state of the LED.
+        boolean bLedOn = true;
+
+        // get a reference to our ColorSensor object.
+        //colorSensor = hardwareMap.get(ColorSensor.class, "sensor_color");
+
+        // Set the LED in the beginning
+        colorSensor.enableLed(bLedOn);
+
+        //
         waitForStart();
         sensor_sleep.reset();
 
         while (opModeIsActive()) {
-
             double koef = 1.;
             double y = -gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x;
@@ -42,6 +71,7 @@ public class NewTele extends LinearOpMode {
             double rotate = gamepad1.right_trigger - gamepad1.left_trigger;
             JoyDir.set(y, -x);
             translational.set(JoyDir.turn(Robot.IMU.getPositiveAngle() + 2 * PI));
+            Color.RGBToHSV(colorSensor.red() * 8, colorSensor.green() * 8, colorSensor.blue() * 8, hsvValues);
             if (gamepad1.a) koef = 2.5;
             else if (gamepad1.b) koef = 1.;
             else koef = 10. / 7.;
@@ -54,13 +84,13 @@ public class NewTele extends LinearOpMode {
             } else {
                 new_thread.EL.Apply_Speed_for_Elevator(y_el);
 
-                if (gamepad2.b) {
+                if (gamepad2.right_trigger > 0.1) {
                     if (Robot.servo.getPosition() < 0.9) {
                         telemetry.addData("!", "!");
                         led_flag = 2;
                     }
                     Robot.servo.setPosition(1.);
-                } else if (gamepad2.x) {
+                } else if (gamepad2.left_trigger > 0.1) {
                     Robot.servo.setPosition(0.8);
                     sensor_sleep.reset();
                 } else if (gamepad2.a) {
@@ -123,6 +153,11 @@ public class NewTele extends LinearOpMode {
                     new_thread.change_per(0);
                 }
                 telemetry.addData("Lift Enc", new_thread.getPos());
+                telemetry.addData("Clear", colorSensor.alpha());
+                telemetry.addData("Red  ", colorSensor.red());
+                telemetry.addData("Green", colorSensor.green());
+                telemetry.addData("Blue ", colorSensor.blue());
+                telemetry.addData("Hue", hsvValues[0]);
                 telemetry.update();
             }
 
